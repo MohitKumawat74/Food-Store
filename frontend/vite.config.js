713f0361417ce843ab3dba84ffacc74dfe8a8869
Mohit Kumawat <mohitkumawat5353@gmail.com>
2025-04-30 14:46:42 +0530

    Fix bcrypt issue for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://food-store-backend-jm6i.onrender.com",
         changeOrigin: true,
         secure: false,
       },
